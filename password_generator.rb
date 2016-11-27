7929aa1c101b30d714befd443d94c7e8c7711f84
izaakf <zakfreeman66@gmail.com>
2016-11-27 06:08:34 +0000

    working password generator

--- password_generator.rb
+++ password_generator.rb
@@ -1,9 +1,16 @@
 # Made by Zak Freeman
-character_list = [*?0..?z]
-def random_password_generator(character_list)
 
-  password = character_list[rand(75)]
-  puts password
+character_list = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ":", ";", "<", "=", ">", "?", "@", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "[", "\\", "]", "^", "_", "`", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+def password_length(character_list)
+  pass_length = [(print 'What length do you want your password to be?'), gets.to_i][1]
+  password = ''
+
+  pass_length.times do
+    password += character_list[rand(75)]
+  end
+
+  print password
 end
 
-random_password_generator(character_list)
+password_length(character_list)
