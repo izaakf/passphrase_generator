7f86a10cef9be0d956ef447da26c91ece6430b44
izaakf <zakfreeman66@gmail.com>
2016-11-28 02:11:30 +0000

    working version. allows user to create rules for their password. uses hashes to store character types

--- password_generator.rb
+++ password_generator.rb
@@ -1,16 +1,56 @@
 # Made by Zak Freeman
 
-character_list = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ":", ";", "<", "=", ">", "?", "@", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "[", "\\", "]", "^", "_", "`", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+# converts input to true/false
+class String
+  def to_b
+    ["y", "yes", "Y", "YES"].include?(self.chomp.downcase)
+  end
+end
 
-def password_length(character_list)
-  pass_length = [(print 'What length do you want your password to be?'), gets.to_i][1]
+# all possible characters in password removed certain symbols so that the passwords are easier to type/remember
+character_list = {
+    lowercase_letters: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
+    uppercase_letters: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
+    numbers: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
+    symbols: ["!", "#", "$", "%", "&", "(", ")", "*", "+", "-", "<", "=", ">", "@", "{", "}", "^"],
+
+}
+
+# possible character types in password
+password_combinations = [:lowercase_letters, :uppercase_letters, :numbers, :symbols]
+
+# creates rules for the password
+
+def character_selection(password_combinations)
+  questions = ["do you want lowercase letters in your password? ", "do you want uppercase letters in your password? ", "do you want numbers in your password? ", "do you want symbols in your password? (e.g [, <, {, &) "]
+  array_pos = 0
+  4.times do |x|
+    # asks user what type of characters they want in their password
+    print questions[x]
+    answer = gets.chomp.to_b
+
+    if answer != true
+      password_combinations.delete(password_combinations[x - array_pos])
+      array_pos +=1
+    end
+
+  end
+end
+
+def password_generator(character_list,password_combinations)
   password = ''
 
-  pass_length.times do
-    password += character_list[rand(75)]
-  end
+  # prompts user input
+  print 'What length do you want your password to be? '
+  pass_length = gets.to_i
 
+  pass_length.times do
+    # adds a random character from the users selection of character types user specified number of times
+    password.concat(character_list[password_combinations.sample].sample)
+  end
+  
   print password
 end
 
-password_length(character_list)
+character_selection(password_combinations)
+password_generator(character_list,password_combinations)
