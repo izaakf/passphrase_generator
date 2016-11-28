8ba6f7d3ba9a6ee5036dd38904a3296a0615de2a
Zak Freeman <zakfreeman66@gmail.com>
2016-11-28 02:23:52 +0000

    Update password_generator.rb

--- password_generator.rb
+++ password_generator.rb
@@ -7,7 +7,7 @@ class String
   end
 end
 
-# all possible characters in password removed certain symbols so that the passwords are easier to type/remember
+# all possible characters in password. removed certain symbols so that the generated passwords are easier to type/remember
 character_list = {
     lowercase_letters: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
     uppercase_letters: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
