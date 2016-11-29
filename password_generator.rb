5b1a6ce48c41f5123f738d91daf793f918af5d87
izaakf <zakfreeman66@gmail.com>
2016-11-29 15:52:56 +0000

    changed A-Z to A..Z

--- password_generator.rb
+++ password_generator.rb
@@ -9,9 +9,9 @@ end
 
 # all possible characters in password. removed certain symbols so that the generated passwords are easier to type/remember
 character_list = {
-    lowercase_letters: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
-    uppercase_letters: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
-    numbers: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
+    lowercase_letters: [*?a..?z],
+    uppercase_letters: [*?A..?Z],
+    numbers: [*?0..?9],
     symbols: ["!", "#", "$", "%", "&", "(", ")", "*", "+", "-", "<", "=", ">", "@", "{", "}", "^"],
 
 }
